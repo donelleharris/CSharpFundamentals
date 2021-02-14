be9c114b9e9d3dd714a3cab7388774446faeba0b
Donelle Harris <donelle.harris@gmail.com>
2021-02-14 11:08:34 -0600

    Complete DateTime & TimeSpan lecture

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -8,152 +8,37 @@ namespace CSharpFundamentals
     {
         public static void Main(string[] args)
         {
-            /*
-             When you post a message on Facebook, depending on the number of 
-             people who like your post, Facebook displays different information.
-                * If no one likes your post, it doesn't display anything.
-                
-                * If only one person likes your post, it displays: [Friend's Name] 
-                  likes your post.
-                * If two people like your post, it displays: [Friend 1] and 
-                  [Friend 2] like your post.
-                * If more than two people like your post, it displays: [Friend 1], 
-                  [Friend 2] and [Number of Other People] others like your post.
+            //Dates & Time in C#
+            
+            var dateTime = new DateTime(2021, 2, 14);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+            Console.WriteLine("Today: " + today);
+            Console.WriteLine("Hour: " + now.Hour + " Minute: " + now.Minute);
 
-             Write a program and continuously ask the user to enter different names,
-             until the user presses Enter (without supplying a name). Depending on
-             the number of names provided, display a message based on the above pattern.
-             */
-            //var friendsList = new List<string>();
+            //roll forward/back a day:
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+            Console.WriteLine("");
+            Console.WriteLine("ToLongDateString: " + now.ToLongDateString());
+            Console.WriteLine("ToShortDateString: " + now.ToShortDateString());
+            Console.WriteLine("ToLongTimeString: " + now.ToLongTimeString());
+            Console.WriteLine("ToShortTimeString: " + now.ToShortTimeString());
+            Console.WriteLine("ToString in dd-MMM-yyyy format: " + now.ToString("dd-MMM-yyyy"));
 
-            //while (true)
-            //{
-            //    Console.WriteLine("Enter friend's name: ");
-            //    var friend = Console.ReadLine();
-            //    if (friend != "")
-            //    {
-            //        friendsList.Add(friend);
-            //        continue;
-            //    }
-            //    else break;
-            //}
-            //switch (friendsList.Count)
-            //{
-            //    case 1:
-            //        Console.WriteLine(friendsList[0] + " likes your post.");
-            //        break;
-            //    case 2:
-            //        Console.WriteLine(friendsList[0] + " & " + friendsList[1] + " like your post.");
-            //        break;
-            //    default:
-            //        Console.WriteLine(friendsList[0] + ", " + friendsList[1] + " & " + (friendsList.Count - 2) + " others like your post.");
-            //        break;
+            //TimeSpan
+            var timeSpan = new TimeSpan(1, 2, 3);//days, hours, minutes
 
-            //}
+            var start = DateTime.Now;
+            var end = start.AddMinutes(120);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
 
-            /*
-             Write a program and ask the user to enter their name. Use an array 
-             to reverse the name and then store the result in a new string.
-             Display the reversed name on the console.
-            */
-            //Console.WriteLine("Please enter your name: ");
-            //var userName = Console.ReadLine();
-            //char[] nameArray = userName.ToCharArray();
-            //Array.Reverse(nameArray);
-            //Console.WriteLine(new string(nameArray));
+            //properties
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
 
-            /*
-             Write a program and ask the user to enter 5 numbers. If a number has
-             been previously entered, display an error message and ask the user
-             to re-try. Once the user successfully enters 5 unique numbers, sort
-             them and display the result on the console.
-            */
-            //var userNumbers = new List<int>();
-            //while(userNumbers.Count < 5)
-            //{
-            //    Console.WriteLine("Please enter a number: ");
-            //    var input = Convert.ToInt32(Console.ReadLine());
-            //    if (userNumbers.Contains(input))
-            //    {
-            //        Console.WriteLine(input + " has already been entered.");
-            //        continue;
-            //    }
-            //    userNumbers.Add(input);
-            //}
-            //userNumbers.Sort();
-            //Console.WriteLine("");
-            //Console.Write("Your numbers were: ");
-            //var numberArray = new Array[userNumbers.Count];
-            //foreach(var number in userNumbers)
-            //{
-            //    Console.Write(number + " ");
-            //}
-
-            /*
-             Write a program and ask the user to continuously enter a number or
-             type "Quit" to exit. The list of numbers may include duplicates.
-             Display the unique numbers that the user has entered.
-            */
-            //var list = new List<string>();
-            //Console.WriteLine("Please enter a number or \"quit\" to exit: ");
-            //var input = (Console.ReadLine());
-            //while(input != "quit")
-            //{
-            //    list.Add(input);
-            //    Console.WriteLine("Please enter a number or \"quit\" to exit: ");
-            //    input = (Console.ReadLine());
-            //}
-            //var uniqueList = new HashSet<string>(list);
-            //foreach(string u in uniqueList)
-            //{
-            //    Console.WriteLine(u);
-            //}
-
-            /*
-             Write a program and ask the user to supply a list of comma separated
-             numbers (e.g 5, 1, 9, 2, 10). If the list is empty or includes less
-             than 5 numbers, display "Invalid List" and ask the user to re-try;
-             otherwise, display the 3 smallest numbers in the list.
-            */
-            string[] elements;
-            while (true)
-            {
-                Console.Write("Enter a list of comma-separated numbers: ");
-                var input = Console.ReadLine();
-
-                if (!String.IsNullOrWhiteSpace(input))
-                {
-                    elements = input.Split(',');
-                    if (elements.Length >= 5)
-                        break;
-                }
-
-                Console.WriteLine("Invalid List");
-            }
-
-            var numbers = new List<int>();
-            foreach (var number in elements)
-                numbers.Add(Convert.ToInt32(number));
-
-            var smallests = new List<int>();
-            while (smallests.Count < 3)
-            {
-                // Assume the first number is the smallest
-                var min = numbers[0];
-                foreach (var number in numbers)
-                {
-                    if (number < min)
-                        min = number;
-                }
-                smallests.Add(min);
-
-                numbers.Remove(min);
-            }
-
-            Console.WriteLine("The 3 smallest numbers are: ");
-            foreach (var number in smallests)
-                Console.WriteLine(number);
-
+            //add
         }
     }
 }
