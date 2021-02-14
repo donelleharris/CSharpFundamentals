2bfaa6d7bd798e76264079787f6c1f7a4a7172a6
Donelle Harris <donelle.harris@gmail.com>
2021-02-14 16:10:20 -0600

    Partially complete strings lecture

---
+++ CSharpFundamentals/DateTime.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace CSharpFundamentals
+{
+    public class DateTime
+    {
+        public DateTime()
+        {
+            /*
+                //Dates & Time in C#
+            
+                var dateTime = new DateTime(2021, 2, 14);
+                var now = DateTime.Now;
+                var today = DateTime.Today;
+                Console.WriteLine("Today: " + today);
+                Console.WriteLine("Hour: " + now.Hour + " Minute: " + now.Minute);
+
+                //roll forward/back a day:
+                var tomorrow = now.AddDays(1);
+                var yesterday = now.AddDays(-1);
+                Console.WriteLine("");
+                Console.WriteLine("ToLongDateString: " + now.ToLongDateString());
+                Console.WriteLine("ToShortDateString: " + now.ToShortDateString());
+                Console.WriteLine("ToLongTimeString: " + now.ToLongTimeString());
+                Console.WriteLine("ToShortTimeString: " + now.ToShortTimeString());
+                Console.WriteLine("ToString in dd-MMM-yyyy format: " + now.ToString("dd-MMM-yyyy"));
+
+                //TimeSpan
+                var timeSpan = new TimeSpan(1, 2, 3);//days, hours, minutes
+
+                var start = DateTime.Now;
+                var end = start.AddMinutes(120);
+                var duration = end - start;
+                Console.WriteLine("Duration: " + duration);
+
+                //properties
+                Console.WriteLine("Minutes: " + timeSpan.Minutes);
+                Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+                //add
+                Console.WriteLine("Add example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+                Console.WriteLine("Subtract example: " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
+
+                //.ToString()
+                Console.WriteLine("From TimeSpan to string: " + timeSpan.ToString());
+                //.Parse()
+                Console.WriteLine("From string to TimeSpan: " + TimeSpan.Parse("01:02:03"));
+            */
+        }
+    }
+}
--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -8,37 +8,76 @@ namespace CSharpFundamentals
     {
         public static void Main(string[] args)
         {
-            //Dates & Time in C#
+            /*
+             * Formatting Strings:
+             *    .ToLower() returns string in all lowercase characters
+             *    .ToUpper() returns string in all uppercase characters
+             *    .Trim() trims leading and trailing whitespace characters
+             * Searching Strings:
+             *    .IndexOf() returns the first index of a given character or set of characters in a string
+             *    .LastIndexOf() returns the last index of a given character or set of characters in a string
+             * Substrings:
+             *    .Substring(startIndex) returns the remaining characters of a string from a specific index start point
+             *    .Substring(startIndex, length) returns a set number of characters of a string from a specific index start point
+             * Replace:
+             *    .Replace('a', '!') replaces first character with last character (i.e. all 'a' characters become '!' throughout the string)
+             *    .Replace("mosh", "moshfegh") replaces all instances of "mosh" with "moshfegh" throughout the string
+             * Null checking:
+             *    String.IsNullOrEmpty(str) checks passed string parameter for empty string ""
+             *    String.IsNullOrWhiteSpace(str) checks passed string parameter for empty string or string of only whitespace characters
+             * Splitting:
+             *    str.Split(' ') 
+             * Converting strings to numbers:
+             *    int.Parse()
+             *    Convert.ToInt32() this is the preferred method since it will convert an empty string to 0
+             * Converting numbers to strings:
+             *    .ToString() converts directly to string
+             *    .ToString("C") converts to currency (i.e. $1234.00)
+             *    .ToString("C0") converts to currency without trailing decimal places
+             *        format specifiers: 
+             *          c or C - currency
+             *          d or D - decimal
+             *          e or E - exponential
+             *          f or F - fixed point
+             *          x or X - hexadecimal
+             */
+
+            //var fullName = "Donelle Harris ";
+            //Console.WriteLine("Without Trim: '{0}'", fullName);
+            //Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            //Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+            //var index = fullName.IndexOf(' ');
+            //var firstName = fullName.Substring(0, index);
+            //var lastName = fullName.Substring(index + 1);
+            //Console.WriteLine(firstName);
+            //Console.WriteLine(lastName);
+
+            var sentence = "This is going to be a really really really really really really really really really really long sentence.";
+            var summary = SummarizeText(sentence);
+            Console.WriteLine(summary);
+        }
+        static string SummarizeText(string text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+            {
+                return text;
+            }
             
-            var dateTime = new DateTime(2021, 2, 14);
-            var now = DateTime.Now;
-            var today = DateTime.Today;
-            Console.WriteLine("Today: " + today);
-            Console.WriteLine("Hour: " + now.Hour + " Minute: " + now.Minute);
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
 
-            //roll forward/back a day:
-            var tomorrow = now.AddDays(1);
-            var yesterday = now.AddDays(-1);
-            Console.WriteLine("");
-            Console.WriteLine("ToLongDateString: " + now.ToLongDateString());
-            Console.WriteLine("ToShortDateString: " + now.ToShortDateString());
-            Console.WriteLine("ToLongTimeString: " + now.ToLongTimeString());
-            Console.WriteLine("ToShortTimeString: " + now.ToShortTimeString());
-            Console.WriteLine("ToString in dd-MMM-yyyy format: " + now.ToString("dd-MMM-yyyy"));
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
 
-            //TimeSpan
-            var timeSpan = new TimeSpan(1, 2, 3);//days, hours, minutes
-
-            var start = DateTime.Now;
-            var end = start.AddMinutes(120);
-            var duration = end - start;
-            Console.WriteLine("Duration: " + duration);
-
-            //properties
-            Console.WriteLine("Minutes: " + timeSpan.Minutes);
-            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
-
-            //add
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                    break;
+            }
+            return String.Join(" ", summaryWords) + "...";
+            
+            
         }
     }
 }
