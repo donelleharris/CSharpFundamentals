93a0ec28eec964dfc2210a2f0274ce069bc26380
Donelle Harris <donelle.harris@gmail.com>
2021-02-15 22:20:20 -0600

    Continue String lecture

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -52,32 +52,12 @@ namespace CSharpFundamentals
             //Console.WriteLine(firstName);
             //Console.WriteLine(lastName);
 
-            var sentence = "This is going to be a really really really really really really really really really really long sentence.";
-            var summary = SummarizeText(sentence);
-            Console.WriteLine(summary);
-        }
-        static string SummarizeText(string text, int maxLength = 20)
-        {
-            if (text.Length < maxLength)
-            {
-                return text;
-            }
-            
-            var words = text.Split(' ');
-            var totalCharacters = 0;
-            var summaryWords = new List<string>();
+            //var sentence = "This is going to be a really really really really really really really really really really long sentence.";
+            //var summary = StringUtility.SummarizeText(sentence, 25);
+            //Console.WriteLine(summary);
 
-            foreach (var word in words)
-            {
-                summaryWords.Add(word);
 
-                totalCharacters += word.Length + 1;
-                if (totalCharacters > maxLength)
-                    break;
-            }
-            return String.Join(" ", summaryWords) + "...";
-            
-            
         }
+        
     }
 }
